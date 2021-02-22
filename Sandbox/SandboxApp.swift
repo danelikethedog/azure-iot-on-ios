fb975ac0b3a2035fe4d22d47945cc282b0ce939c
Dane Walton <dane.walton@microsoft.com>
2021-02-22 13:47:34 -0800

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Dane Walton on 2/22/21.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
